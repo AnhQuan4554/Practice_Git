d356fdcb576b5e77c58ea062895f742afc7d3f28
Quân <quanmanchester0405@gmail.com>
2023-10-26 09:54:12 +0700

    test change 1
    tesst change 2
    
    test change 4
    
    test change 5
    
    test change 6
    
    feature 1
    
    feature 2 update new code

--- home.js
+++ home.js
@@ -1,1 +1,16 @@
-const testUpdate = "update4";
+const arrayCoupon = ["ReferralNTUC"];
+const regex = /[^+/]+(?=[+/]|$)/;
+const string1 = "28 Days Double MMJan23-COMBO A+2 PTV/$288+ $1302";
+const string2 = "28 Days Double MMJan23-COMBO B/$288+ $1302";
+////
+const main_and_promocode = string1.match(regex)[0];
+const isMatch = arrayCoupon.find((coupon) =>
+  main_and_promocode.includes(coupon)
+);
+
+if (isMatch) {
+  console.log("Promo code is");
+  console.log(isMatch);
+} else {
+  console.log("Promo code doesn't exit");
+}
